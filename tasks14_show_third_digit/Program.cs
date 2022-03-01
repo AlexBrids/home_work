ec44b64f92117c05b01ceae1537e0fec97ff7900
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-01 15:03:00 +0300

    add tasks 14 show third digit

---
+++ tasks14_show_third_digit/Program.cs
@@ -0,0 +1,33 @@
+﻿// Показать третью цифра числа
+
+Console.Clear();
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(50, 150);
+    return randomNumber;
+}
+
+int number = GetNumber();
+Console.WriteLine($"Исходное число: {number}");
+
+
+void ShowThirdDigit(int count)
+{
+    string str = Convert.ToString(number);
+    int length = str.Length;
+
+    if (length < 3)
+    {
+        Console.WriteLine($"У числа всего 2 цифры");
+
+    }
+    else
+    {
+        int result = count % 10;
+        Console.WriteLine($"Третья цифра числа: {result}");
+    }
+
+}
+
+ShowThirdDigit(number);
