56bcd9d93d3f78fd83ec33bdb4788e334b236f1e
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-03 22:03:39 +0300

    add tasks 21 palindrom

---
+++ tasks21_palindrom/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+
+Console.Write($"Введите пятизначное число: ");
+string getNumber = Console.ReadLine();
+
+
+void Palindrom(string number)
+{
+    if(number.Length == 5)
+    {
+        if((number[0] == number[4]) && (number[1] == number[3]))
+        {
+        Console.WriteLine($"Число {number} является палиндромом");
+        }
+        else
+        {
+        Console.WriteLine($"Число {number}  не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Перезапустите программу и введите число повторно!!!");
+    }
+
+    
+}
+
+
+Palindrom(getNumber);
