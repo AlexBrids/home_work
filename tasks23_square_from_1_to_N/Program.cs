9741171abdc85b2c9ed6282f752a6f6257666c4c
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-03 22:22:24 +0300

    add tasks 23 aquare from 1 to N
    Signed-off-by: Бридский Александр <bridskiy@JUPITER9.RU>

---
+++ tasks23_square_from_1_to_N/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+
+using static System.Console;
+Clear();
+
+int GetNumber()
+{
+    int randNumber = new Random().Next(5, 10);
+    return randNumber;
+}
+
+int N = GetNumber();
+WriteLine($"Пограничное значение равно {N}");
+
+/*
+for(int i = 1; i <= N; i++)
+{
+    Write($"{i} * {i} = {i * i}");
+    WriteLine();
+}
+*/
+for (int i = 1; i <= N; i++)
+{
+    Write($"Квадрат числа {i} равен {Math.Pow(i, 2)}");
+    WriteLine();
+}
