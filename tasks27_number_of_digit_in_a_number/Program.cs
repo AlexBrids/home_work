de736f4e7a005404666276d1fd36e53c0bf9e881
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-03 22:39:46 +0300

    add tasks 27 number of digit in number

---
+++ tasks27_number_of_digit_in_a_number/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе
+
+using static System.Console;
+Clear();
+
+int GetNumber()
+{
+    int randNumber = new Random().Next(1, 100000);
+    return randNumber;
+}
+
+int number = GetNumber();
+WriteLine($"Случайное число: {number}");
+
+string text = Convert.ToString(number);
+int number_of_digit = text.Length;
+WriteLine($"Количество цифр в числе равно: {number_of_digit}");
