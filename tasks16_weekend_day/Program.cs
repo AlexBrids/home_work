bbe385e995d6f3767a3efb2c3c9ec15fcc09e1bc
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-03 20:49:18 +0300

    asssa

---
+++ tasks16_weekend_day/Program.cs
@@ -0,0 +1,26 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+Console.Clear();
+
+int GetDayNumber()
+{
+    int getDayNumber = new Random().Next(1, 8);
+    return getDayNumber;
+}
+
+int dayNumber = GetDayNumber();
+Console.WriteLine($"Номер дня недели: {dayNumber}");
+
+void WeekdayOrWeekend(int numberOfDay, int dayNumber1, int dayNumber2)
+{
+    if((numberOfDay == dayNumber1) || (numberOfDay == dayNumber2))
+    {
+        Console.WriteLine($"Номер дня недели соответствует выходному дню!");
+    }
+    else
+    {
+        Console.WriteLine("Номер дня неделе соответствует рабочему дню!");
+    }
+}
+
+WeekdayOrWeekend(dayNumber, 6, 7);
