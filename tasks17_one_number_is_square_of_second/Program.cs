c9286d934a60ad46b834fd47c89b709d8a14ec46
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-03 21:25:12 +0300

    add tasks 17 one number is square of second

---
+++ tasks17_one_number_is_square_of_second/Program.cs
@@ -0,0 +1,29 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Clear();
+
+int GetNumber(int limit)
+{
+    int ranNumber = new Random().Next(1, limit);
+    return ranNumber;
+}
+
+int firstNumber = GetNumber(10);
+Console.WriteLine($"Первое число равно: {firstNumber}");
+
+int secondNumber = GetNumber(4);
+Console.WriteLine($"Второе число равно: {secondNumber}");
+
+void FirstSquareSecond (int first, int second)
+{
+    if(first / second == second)
+    {
+        Console.WriteLine("Первое число является квадратом второго!");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не является квадратом второго!");
+    }
+}
+
+FirstSquareSecond(firstNumber, secondNumber);
