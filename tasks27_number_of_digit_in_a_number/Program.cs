183ef5f1b66695d7fe865291692bbff9998b18d2
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-04 16:30:35 +0300

    add new task in task 27

--- tasks27_number_of_digit_in_a_number/Program.cs
+++ tasks27_number_of_digit_in_a_number/Program.cs
@@ -2,7 +2,7 @@
 
 using static System.Console;
 Clear();
-
+/*
 int GetNumber()
 {
     int randNumber = new Random().Next(1, 100000);
@@ -15,3 +15,53 @@ WriteLine($"Случайное число: {number}");
 string text = Convert.ToString(number);
 int number_of_digit = text.Length;
 WriteLine($"Количество цифр в числе равно: {number_of_digit}");
+*/
+
+
+// Вариант 2 Количество цифр в строке
+
+
+string text = "kjlkj54k55hjkh5879";
+
+
+async void NumberOfDigitInWord(string str)
+{
+    int numberOfDigit = 0;
+    int length = str.Length;
+    int i = 0;
+    for (i = 0; i < length; i++)
+    {
+        char[] arr = str.ToCharArray();
+        
+        if((arr[i] == '0') || (arr[i] == '1') || (arr[i] == '2') || (arr[i] == '3') || (arr[i] == '4') || (arr[i] == '5') || (arr[i] == '6') || (arr[i] == '7') || (arr[i] == '8') || (arr[i] == '9'))
+        {
+            numberOfDigit++;
+            //Write("2");
+        }
+        else
+        {
+           
+        }
+        
+        
+    }
+    int result = numberOfDigit;
+    Write(result);
+}
+
+NumberOfDigitInWord(text);
+
+/*
+string str = "kjlkj54k55hjkh5879";
+char[] arr = str.ToCharArray();
+
+if (arr[1] == 'j')
+{
+    Write("1");
+}
+else
+{
+    Write("2");
+}
+*/
+
