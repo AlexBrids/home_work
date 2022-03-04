93fb8d6f15872191d76695ec4f5f653335bcc4c6
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-04 16:40:09 +0300

    add new answer tasks 27

--- tasks27_number_of_digit_in_a_number/Program.cs
+++ tasks27_number_of_digit_in_a_number/Program.cs
@@ -21,7 +21,7 @@ WriteLine($"Количество цифр в числе равно: {number_of_d
 // Вариант 2 Количество цифр в строке
 
 
-string text = "kjlkj54k55hjkh5879";
+string text = "kjlkj5";
 
 
 async void NumberOfDigitInWord(string str)
@@ -32,8 +32,16 @@ async void NumberOfDigitInWord(string str)
     for (i = 0; i < length; i++)
     {
         char[] arr = str.ToCharArray();
-        
-        if((arr[i] == '0') || (arr[i] == '1') || (arr[i] == '2') || (arr[i] == '3') || (arr[i] == '4') || (arr[i] == '5') || (arr[i] == '6') || (arr[i] == '7') || (arr[i] == '8') || (arr[i] == '9'))
+        char[] MyArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        for(int j = 0; j < MyArray.Length; j++)
+        {
+            if(arr[i] == MyArray[j])
+            {
+                numberOfDigit++;
+            }
+        }
+
+        /*if((arr[i] == '0') || (arr[i] == '1') || (arr[i] == '2') || (arr[i] == '3') || (arr[i] == '4') || (arr[i] == '5') || (arr[i] == '6') || (arr[i] == '7') || (arr[i] == '8') || (arr[i] == '9'))
         {
             numberOfDigit++;
             //Write("2");
@@ -42,7 +50,7 @@ async void NumberOfDigitInWord(string str)
         {
            
         }
-        
+        */
         
     }
     int result = numberOfDigit;
