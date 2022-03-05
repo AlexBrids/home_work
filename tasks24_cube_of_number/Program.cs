81878c57889099a202d32b44d464dbbd4d47db82
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-05 15:20:02 +0300

    add tasks 24 cube of a number

---
+++ tasks24_cube_of_number/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти кубы чисел от 1 до N
+
+using static System.Console;
+Clear();
+
+int GetNumber()
+{
+    int randNumber = new Random().Next(1, 8);
+    return randNumber;
+}
+
+int N = GetNumber();
+WriteLine(N);
+
+async void CubeOfNumber(int number)
+{
+    for (int i = 0; i <= number; i++)
+    {
+        double cubeNumber = Math.Pow(i, 3);
+        Write($"{cubeNumber} ");
+        
+    }
+    
+}
+CubeOfNumber(N);
