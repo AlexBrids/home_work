9c1ff60908e2973e903446b2dfe39c704b2d8235
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-05 14:03:08 +0300

    add tasks 20 quarter coordinate

---
+++ tasks20_quarter_coordinate/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+using static System.Console;
+Clear();
+
+int GetNumber()
+{
+    int randNumber = new Random().Next(1, 5);
+    return randNumber;
+}
+
+int numbQuator = GetNumber();
+WriteLine($"Номер четверти: {numbQuator}");
+
+void QuarterCoordinate(int number)
+{
+    if(number == 1)
+    {
+        WriteLine("Координаты для первой четверти по Х от 0 до +бесконечности, по У от 0 до +бесконечности");
+    }
+    else if(number == 2)
+    {
+        WriteLine("Координаты для первой четверти по Х от 0 до +бесконечности, по У от 0 до -бесконечности");
+    }
+    else if(number == 3)
+    {
+        WriteLine("Координаты для первой четверти по Х от 0 до -бесконечности, по У от 0 до -бесконечности");
+    }
+    else
+    {
+        WriteLine("Координаты для первой четверти по Х от 0 до -бесконечности, по У от 0 до +бесконечности");
+    }
+}
+
+QuarterCoordinate(numbQuator);
