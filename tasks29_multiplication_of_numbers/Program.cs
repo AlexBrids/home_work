148526c1a87bcb1e9db7bb54b43d5ac5560d2c78
Бридский Александр <bridskiy@JUPITER9.RU>
2022-03-23 21:33:52 +0300

    add tasks 29 multiplication of numbers

---
+++ tasks29_multiplication_of_numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+using static System.Console;
+Clear();
+
+int GetNumber()
+{
+    int randNumber = new Random().Next(1, 10);
+    return randNumber;
+}
+
+int number = GetNumber();
+WriteLine(number);
+
+async void MultiplicationNumber(int randNumber)
+{
+    int i = 1;
+    int result = 1;
+    for (i = 1; i <= randNumber; i++)
+    {
+        result = result * i;
+    }
+    WriteLine($"Произведение чисел от 1 до {randNumber} равно {result}");
+}
+
+
+MultiplicationNumber(number);
