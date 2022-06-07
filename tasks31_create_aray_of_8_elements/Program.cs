4f3fb2be1dbcc5400883e6049ff5b2ab857e6e16
Бридский Александр <bridskiy@JUPITER9.RU>
2022-06-07 17:25:06 +0300

    add tasks 31 create array of 8 elements

---
+++ tasks31_create_aray_of_8_elements/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+using static System.Console;
+Clear();
+
+void PrintArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} ");
+        }
+    }
+}
+
+
+void FillArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[1, 8];
+
+PrintArray(matrix);
+FillArray(matrix);
+WriteLine();
+PrintArray(matrix);
+
