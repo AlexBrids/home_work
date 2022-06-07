d940abc1edac3d8360daccc50b4dbb8bd9beb0b7
Бридский Александр <bridskiy@JUPITER9.RU>
2022-06-07 17:49:50 +0300

    add tasks 32 create array of 8 elements of 0 and 1

---
+++ tasks32_create_array_of_8_elements_0_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать массив из 8 элементов заполненых 0 и 1 
+
+using static System.Console;
+Clear();
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} ");
+        }
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 2);
+        }
+    }
+    
+}
+
+int[,] matrix = new int[1, 8];
+
+Write($"Пустая матрица: ");
+PrintArray(matrix);
+FillArray(matrix);
+WriteLine();
+Write($"Заполенная матрица: ");
+PrintArray(matrix);
